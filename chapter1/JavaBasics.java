63fb8634f4fb6a865c46bd99fc4304d2cd63d5c4
jinjin <michaeljamesraya2005@gmail.com>
2025-06-15 23:38:10 +0000

    code update

--- chapter1/JavaBasics.java
+++ chapter1/JavaBasics.java
@@ -1,18 +1,32 @@
 package chapter1;
-import java.util.Random;
+import java.util.*;
+// import java.util.Random;
 /**
  * this is a sample class for Ciicc Batch3
  */
  
 public class JavaBasics{
     public static void main(String[] args){
-        System.out.println("Hello World!");
+        System.out.println("Hello World!" );
         Random r = new Random();
-        System.out.println(r.nextInt());
+        // System.out.println(r.nextInt());
 
         //this is a single line comment
         /**
-         * this is a multi line
+         * this is 
+         * a multi line
          */
     }
+}
+
+class Car{
+    int year = 2005;
+    String Name = "misyubibi";
+    String Color = "penk";
+}
+
+class Basics2{
+    JavaBasics mb;
+    Car car1 = new Car();
+    Car car2 = new Car();
 }
