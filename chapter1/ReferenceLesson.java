eb305f1c3058827b3ecb605139ab3327563e184f
jinjin <michaeljamesraya2005@gmail.com>
2025-06-18 03:56:14 +0000

    day12

---
+++ chapter1/ReferenceLesson.java
@@ -0,0 +1,9 @@
+package chapter1;
+
+public class ReferenceLesson {
+    public static void main(String[] args) { // can use other than args
+        java.util.Date today = new java.util.Date();
+        String greetings = "How are you!";
+        System.out.println(args[1]);
+    }
+}
